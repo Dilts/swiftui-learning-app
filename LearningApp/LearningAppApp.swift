4542a00eb4fa400b9a2ce52758fa3822ea379970
Brian Dilts <brianedilts@gmail.com>
2021-04-12 19:32:52 -0600

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Brian Dilts on 4/12/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
