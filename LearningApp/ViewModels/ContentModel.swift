9c9a75659aaaa63834a8ffcc79418c16818d90a3
Brian Dilts <brianedilts@gmail.com>
2021-04-14 13:17:05 -0600

    - add ContentView - generate scrollview of lessons - add ContentViewRow - make each card dynamically and pass to ContentView

--- LearningApp/ViewModels/ContentModel.swift
+++ LearningApp/ViewModels/ContentModel.swift
@@ -9,7 +9,13 @@ import Foundation
 
 class ContentModel: ObservableObject {
     
+    // List of modules
     @Published var modules = [Module]()
+    
+    // Current module
+    @Published var currentModule: Module?
+    var currentModuleIndex = 0
+    
     
     var styleData: Data?
     
@@ -18,7 +24,7 @@ class ContentModel: ObservableObject {
         getLocalData()
         
     }
-    
+    // MARK: - Data Methods
     func getLocalData() {
         
         // Get a url to the json file
@@ -56,4 +62,24 @@ class ContentModel: ObservableObject {
         }
         
     }
+        
+    // MARK: - Module navigation methods
+    
+    func beginModule(_ moduleid:Int) {
+        
+        // Find the index for this module id
+        for index in 0..<modules.count {
+            
+            if modules[index].id == moduleid {
+                
+                // Found the matching module
+                currentModuleIndex = index
+                break
+            }
+        }
+        
+        // Set the current module
+        currentModule = modules[currentModuleIndex]
+    }
+    
 }
